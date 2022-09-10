3959cf3eb94c93dbd1a145b9ae2b933bfb0ed1ec
Roman Efimov <marder977@gmail.com>
2022-09-10 15:56:31 +0500

    Код создает кол-во квадратов раное сроку жизни

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 result.onclick=function(){
-    result.style.backgroundColor="green"
-    let dateOfBirth=document.getElementsByName("dateOfBirth")[0].value
+    startWrapper.style.height="0px"
+    let dateOfBirth=new Date(document.getElementsByName("dateOfBirth")[0].value)
     let Lifetime=document.getElementsByName("Lifetime")[0].value
     let achievedGoal=document.getElementsByName("achievedGoal")[0].value
     let currentGoal=document.getElementsByName("currentGoal")[0].value
     let futureGoal=document.getElementsByName("futureGoal")[0].value
+    let z=0;
+    let diffTime=new Date(dateOfBirth.valueOf()+(1000*60*60*24*360*Lifetime))
 
-    console.log(dateOfBirth);
-    console.log(Lifetime);
-    console.log(achievedGoal);
-    console.log(currentGoal);
-    console.log(futureGoal);
-
-    let diffTime=dateOfBirth+(Lifetime)
+    for(var j=1 ,i=dateOfBirth.valueOf() ;i<diffTime.valueOf();i+=(1000*60*60*24*7), j++){
+        let newRectangle=document.createElement("div");
+        newRectangle.title=j+"-я неделя"
+        flexArea.appendChild(newRectangle);
+        if(i<Date.now())
+        flexArea.children[(j-1)].style.border="1px solid red"
+        else if(z==0 && i>Date.now()){
+            z++
+            flexArea.children[(j-1)].style.border="3px solid blue"
+            newRectangle.title+=". Вы находитесь сдесь."
+        }
+    }
+    
 }
